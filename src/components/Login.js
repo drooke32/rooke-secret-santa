a58b5e73e138af981e60b2363bfac0bc9669e17a
Denis Rooke <denis@coconutcalendar.com>
2017-11-12 21:49:22 -0600

    added login page content and styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import '../css/Login.css';
+
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 class Login extends Component {
   render() {
     return (
-      <h1> The Login Page! </h1>
+      <Card className='card'>
+        <CardHeader title="Secret Santa Login"/>
+        <CardText>
+          <TextField floatingLabelText="Email" fullWidth={true} />
+          <br />
+          <TextField floatingLabelText="Password" type="password" fullWidth={true} />
+        </CardText>
+        <CardActions className="action-container">
+          <RaisedButton label="Login" primary={true} className='login-button' />
+          <FlatButton label="Forgot Password" className='forgot-button' />
+        </CardActions>
+      </Card>
     );
   }
 }
