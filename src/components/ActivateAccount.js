561216b7724025b7dc96662c69806a7a3682d86b
Denis Rooke <denis@coconutcalendar.com>
2017-11-14 13:32:01 -0600

    added form validation for activate page

--- src/components/ActivateAccount.js
+++ src/components/ActivateAccount.js
@@ -24,16 +24,145 @@ const items = [
 
 class ActivateAccount extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      confirm: '',
+      formValidation: {
+        nameError: '',
+        nameValid: false,
+        emailError: '',
+        emailValid: false,
+        passwordError: '',
+        passwordValid: false,
+        confirmError: '',
+        confirmValid: false,
+      },
+    };
+  }
+
+  handleChange = (event, index, value) => {
+    const field = event.target.name || 'name';
+    let targetValue = event.target.value || '';
+
+    if (field === 'name') {
+      targetValue = value;
+    }
+
+    this.setState({
+      [field]: targetValue,
+    });
+
+    this.validateField(field, targetValue)
+  };
+
+  validateField(field, targetValue) {
+    switch(field) {
+      case 'email':
+        this.validateEmail(targetValue);
+        break;
+      case 'password':
+        this.validatePassword(targetValue);
+        break;
+      case 'confirm':
+        this.validateConfirm(targetValue);
+        break;
+      default:
+        this.validateName(targetValue);
+        break;
+    }
+  }
+
+  validateName(value) {
+    const formValidation = {...this.state.formValidation};
+    formValidation['nameValid'] = false;
+    formValidation['nameError'] = 'You must select a name';
+
+    if (value) {
+      formValidation['nameValid'] = true;
+      formValidation['nameError'] = '';
+    }
+
+    this.setState({ formValidation });
+  }
+
+  validateEmail(value) {
+    const formValidation = {...this.state.formValidation};
+    formValidation['emailValid'] = true;
+    formValidation['emailError'] = '';
+
+    if (!value) {
+      formValidation['emailValid'] = false;
+      formValidation['emailError'] = 'You must enter an email';
+    } else if (!this.validEmail(value)) {
+      formValidation['emailValid'] = false;
+      formValidation['emailError'] = 'The email you entered is not a valid format';
+    }
+
+    this.setState({ formValidation });
+  }
+
+  validEmail(value) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(value);
+  }
+
+  validatePassword(value) {
+    const formValidation = {...this.state.formValidation};
+    formValidation['passwordValid'] = false;
+    formValidation['passwordError'] = 'Password must be at least 6 characters long';
+
+    if (value.length >= 6) {
+      formValidation['passwordValid'] = true;
+      formValidation['passwordError'] = '';
+    }
+
+    this.setState({ formValidation });
+  }
+
+  validateConfirm(value) {
+    const formValidation = {...this.state.formValidation};
+    formValidation['confirmValid'] = false;
+    formValidation['confirmError'] = 'The passwords you entered must match ';
+
+    if (this.state.password === value) {
+      formValidation['confirmValid'] = true;
+      formValidation['confirmError'] = '';
+    }
+
+    this.setState({ formValidation });
+  }
+
+  validateForm() {
+    if (this.state.formValidation.nameValid &&
+        this.state.formValidation.emailValid &&
+        this.state.formValidation.passwordValid &&
+        this.state.formValidation.confirmValid) {
+        return true;
+    }
+    return false;
+
+  }
+
   activate(e) {
     e.preventDefault();
 
+    if (!this.validateForm()) {
+      return;
+    }
+
     const person = {
-      name: this.name.value,
-      email: this.email.value,
-      password: this.password.value,
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
     }
 
     console.log(person);
+return;
     //if account not activated
     //and activation succeeds
     //set in the state and navigate
@@ -45,14 +174,49 @@ class ActivateAccount extends React.Component {
       <Card className='card'>
         <CardTitle title="Activate Account" />
         <CardText>
-          <SelectField ref={(input) => this.name = input} floatingLabelText="Name" fullWidth={true} maxHeight={200}>
+          <SelectField 
+            ref={(input) => this.name = input}
+            value={this.state.name}
+            name="name"
+            errorText={this.state.formValidation.nameError}
+            onChange={this.handleChange}
+            floatingLabelText="Name"
+            fullWidth={true}
+            maxHeight={200}
+          >
             {items}
           </SelectField>
-          <TextField ref={(input) => this.email = input} floatingLabelText="Email" fullWidth={true} />
+          <TextField 
+            ref={(input) => this.email = input}
+            value={this.state.email}
+            name="email"
+            errorText={this.state.formValidation.emailError}
+            onChange={this.handleChange}
+            floatingLabelText="Email"
+            fullWidth={true} 
+          />
           <br />
-          <TextField ref={(input) => this.password = input} floatingLabelText="Password" type="password" fullWidth={true} />
+          <TextField 
+            ref={(input) => this.password = input}
+            value={this.state.password}
+            name="password"
+            errorText={this.state.formValidation.passwordError}
+            onChange={this.handleChange}
+            floatingLabelText="Password"
+            type="password"
+            fullWidth={true}
+          />
           <br />
-          <TextField ref={(input) => this.passsword2 = input} floatingLabelText="Confirm Password" type="password" fullWidth={true} />
+          <TextField 
+            ref={(input) => this.passsword2 = input}
+            value={this.state.confirm}
+            name="confirm"
+            errorText={this.state.formValidation.confirmError}
+            onChange={this.handleChange}
+            floatingLabelText="Confirm Password"
+            type="password"
+            fullWidth={true}
+          />
         </CardText>
         <CardActions className="action-container">
           <RaisedButton 
